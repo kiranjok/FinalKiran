41be2695e8b5ad57a3e286fcd65dfe0a61f41c95
kiranjot kiranjot <kiranjok@sheridancollege.ca>
2024-04-15 09:19:57 -0400

    version of student details

--- src/checkstring/CheckString.java
+++ src/checkstring/CheckString.java
@@ -1,31 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package checkstring;
 
-/** This class to checks the given string has any numbers/digits or not.
- * Complete this method and test this method in JUnit.
- * @author srinivsi August 
-  */
+import java.util.regex.Pattern;
+
 public class CheckString {
 
     /**
-     * @param args the command line arguments
+     * Checks if the given string contains any digits.
+     * @param str The string to be checked.
+     * @return true if the string contains digits, false otherwise.
      */
-    public static void main(String[] args) {
-  
-    String c="as23g";
-    if(checkchar(c))
-    {
-        System.out.println("Given String has digits");
+    public static boolean checkchar(String str) {
+        // Regular expression to match any digit
+        String regex = ".*\\d.*";
+        // Check if the string matches the regular expression
+        return Pattern.matches(regex, str);
     }
-    else
-    {
-    System.out.println("Given String has only characters no digits");
-    }
-    }     
 
-  //create method here to check the given String has numbers or not
+    public static void main(String[] args) {
+        // Test the checkchar method
+        String str1 = "as23g";
+        if (checkchar(str1)) {
+            System.out.println("Given String has digits");
+        } else {
+            System.out.println("Given String has only characters, no digits");
+        }
+    }
 }
+
